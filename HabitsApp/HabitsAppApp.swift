d402c919764a670d46c072d45992e36972f73722
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-08 18:33:45 +0300

    Initial Commit

---
+++ HabitsApp/HabitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsAppApp.swift
+//  HabitsApp
+//
+//  Created by Marat Fakhrizhanov on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
