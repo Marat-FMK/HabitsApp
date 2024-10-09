687f042f2e8d1c78f88418c872e297ddc0ca20a3
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-09 16:40:55 +0300

    add view

--- HabitsApp/AddHobit.swift
+++ HabitsApp/AddHobit.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct AddHobit: View {
+    var hobits: Hobits
+    
+    
+    @Environment(\.dismiss) var dismiss
+    @State private var hobitName = "1"
+    @State private var hobitDescription = "3"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            Text("Inter the hobit name:")
+            
+            
+            TextField("hobit name", text: $hobitName)
+                .textFieldStyle(.roundedBorder)
+                .padding(.bottom, 50)
+            
+            Text("Inter hobit description:")
+            
+            TextField("hobit description", text: $hobitDescription)
+            
+        }
+        .padding()
+        
+            Button(action: {
+                
+                hobits.list.append(Hobit(name: hobitName, description: hobitDescription, count: 0))
+                
+                let list = hobits.list
+                
+                guard let data = try? JSONEncoder().encode(list) else {
+                    print("Failed encode")
+                          return }
+                
+                          UserDefaults.standard.set(data, forKey: "hobitsList")
+                
+                
+                
+                
+                dismiss()
+                
+            } ) {
+                Text("save hobit")
+            }
+               
     }
+    
+  
+        
+    
 }
 
 #Preview {
-    AddHobit()
+    AddHobit(hobits: Hobits.init())
 }
