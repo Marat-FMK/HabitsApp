ed659812a983d49da9d2c02ff2ab1fa6fee89bf8
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-09 16:41:39 +0300

    work with model and logic

--- HabitsApp/Hobits.swift
+++ HabitsApp/Hobits.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Hobit: Hashable, Equatable, Codable {
+    let id = UUID()
+    let name: String
+    let description: String
+    var count: Int
+    
+}
+
+
+
+
+@Observable
+class Hobits {
+    
+    
+    var list: [Hobit] = []
+    
+    init() {
+        
+        guard let data = UserDefaults.standard.data(forKey: "hobitsList") else { print("No data"); return}
+        guard let decodeList = try? JSONDecoder().decode([Hobit].self, from: data) else {
+            print("Decode failed")
+            return }
+                
+                self.list = decodeList
+    }
+    
+    
+}
+//        Hobit(name: "play guitar", description: "everytime", count: 0),
+//        Hobit(name: "go gum", description: "4 or 5 times of week", count: 0)
